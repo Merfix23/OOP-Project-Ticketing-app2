304c7561a8c922761e9f977acd49c5b1d4209ef8
raduc <raduc@DESKTOP-IOCCQSQ>
2023-11-21 20:26:12 +0200

    Add project files.

---
+++ OOP Project Ticketing app/OOP Project Ticketing app.cpp
@@ -0,0 +1,21 @@
+// OOP Project Ticketing app.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <string.h>
+using namespace std;
+
+class Location {
+    string name = "";
+    int maxSeats = 0;
+    int rows = 0;
+    enum seatType {VIP, Occupied, Avalable, Disabled, Other};
+
+
+};
+
+int main()
+{
+    
+}
