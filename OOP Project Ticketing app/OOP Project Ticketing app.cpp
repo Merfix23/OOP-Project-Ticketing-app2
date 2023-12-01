8468d64ff3ed2674ab7426f0c7f2648fe896b3b6
Merfu Radu <Merfu@DESKTOP-BSTPNR5>
2023-12-01 21:19:28 +0200

    Adds a new Row class and some functions

--- OOP Project Ticketing app/OOP Project Ticketing app.cpp
+++ OOP Project Ticketing app/OOP Project Ticketing app.cpp
@@ -5,16 +5,52 @@
 
 using namespace std;
 
+class Row {
+    int size = 0;
+    string* seats;
+
+    //Constructor with one parameter
+    Row(int size, int rowNr) : size(size) {
+        seats = new string[size];
+        for (int i = 0; i < size; i++) {
+            seats[i] = i + 1;
+            cout << endl << "seat:" << seats[1];
+        }
+    }
+
+    //function to set the seat availability
+    void setSeat(int index, const string& status) {
+        if (index >= 0 && index < size) {
+            seats[index] = status;
+        }
+        else {
+            throw exception("Invalid seat index");
+        }
+    }
+
+    //function to display the seat status
+    void displaySeats() {
+        for (int i = 0; i < size; ++i) {
+            cout << "Seat " << i + 1 << ": " << seats[i] << endl;
+        }
+    }
+
+    //Destructor
+    ~Row() {
+        delete[] seats;
+    }
+};
+
 class Location {
 private:
     char* name;
     int maxSeats = 0;
     int rows = 0;
-  
+
 public:
     //Default constructor
 
-    Location() :  maxSeats(0), rows(0){}
+    Location() : maxSeats(0), rows(0) {}
     //Constructor with parameters
 
     Location(char* name, int maxSeats, int rows) {
@@ -33,23 +69,23 @@ private:
     double price;
     int ticketNumber;
 
-    
+
 
 public:
     // Default constructor
-    Ticket(char* eventName, int price, int ticketNumber){
+    Ticket(char* eventName, int price, int ticketNumber) {
         this->eventName = new char[strlen(eventName) + 1];
         strcpy(this->eventName, eventName);
         this->price = price;
         this->ticketNumber = ticketNumber;
     }
 
-   
-  
+
+
 };
 
 
 int main()
 {
-    
+
 }
